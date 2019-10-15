21c7bf941c4bc0868971b6333ab13c295db12cc7
Mateussilvasant <mateussilva_sant@yahoo.com.br>
2019-10-15 14:21:53 -0300

    Estrutura Inicial Spring.

---
+++ src/main/java/br/com/suriacaprichos/SuriaCaprichosApplication.java
@@ -0,0 +1,13 @@
+package br.com.suriacaprichos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuriaCaprichosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuriaCaprichosApplication.class, args);
+	}
+
+}
